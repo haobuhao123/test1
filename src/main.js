bede586326a3032dfa7c3f225d767428661e66db
孙悟空 <2391639072@qq.com>
2019-08-02 17:52:05 +0800

    登录请求

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
-import Vue from "vue";
-import App from "./App.vue";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/style/index.less'
+
 // @ 是某一个路径别名  src路径别名  在vue-cli创建的项目下才可使用
 // 目录下是有默认索引文件 index.js 就是索引文件  .js .vue .json
-import router from "@/router";
+import router from '@/router'
+
+import axios from 'axios'
+Vue.prototype.$http = axios
 
 // tip 控制台提示信息，开发环境来提示
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 // main.js 的职责：1.导入项目需要依赖的资源（js包，css文件，其他资源）
 // 2，创建一个根实列
-Vue.use(ElementUI);
+Vue.use(ElementUI)
 // 根实列
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,110 @@
 <template>
-  <div class="container">Login</div>
+  <div class="container">
+    <el-card class="my-card">
+      <img src="../../assets/images/logo_index.png" alt />
+      <!-- 表单 -->
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" status-icon>
+        <!-- 表单项 -->
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号 "></el-input>
+        </el-form-item>
+        <!-- 表单项 -->
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:286px;margin-right:10px;"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <!-- 表单项 -->
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <!-- 表单项 -->
+        <el-form-item>
+          <el-button type="primary" style="width:100%" @click="login()">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data () {
+    // 申明校验函数、
+    const checkMobile = (rule, value, callback) => {
+      // 检验手机号  1开头 第二位 3--9 ，9位数字
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机号格式不对'))
+      }
+      callback()
+    }
+    return {
+      // 表单对象数据
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      // 表单校验规则数据
+      loginRules: {
+        mobile: [
+          // blur 失去焦点
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur ' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '长度是6位', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 对整个表单进行校验
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          // 请求登录接口
+          this.$http
+            .post(
+              'http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm
+            ).then(res => {
+              // res响应对象 包含响应主体
+              console.log(res.data)
+              // 成功跳转到首页
+              this.$router.push('/')
+            }).catch(() => {
+              // 错误提示
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
+    }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  // 背景图的定位  /  背景图尺寸 ，contain 等比缩放完整在容器内显示  ， cover  等比缩放完全铺满容器
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+}
+.my-card {
+  width: 450px;
+  height: 350px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -55%);
+  img {
+    display: block;
+    width: 200px;
+    margin: 0 auto 30px;
+  }
+}
+</style>
